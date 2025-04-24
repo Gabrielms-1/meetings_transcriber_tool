d4e7f030a7e15501d69f842035866dc0a6c7f12b
gabrielms-1 <gabriel.sousa@syngenta.com>
2025-04-24 20:01:14 -0300

    whisper working

--- src/transcript_audio_to_text.py
+++ src/transcript_audio_to_text.py
@@ -1,25 +1,32 @@
-import assemblyai as aai
+import whisper
 import json
 import os
 
-aai.settings.api_key = os.getenv("ASSEMBLYAI_KEY")
+MODEL_NAME = "tiny"
+ASR_MODEL = whisper.load_model(MODEL_NAME, device="cpu")
 
 def transcript_audio_to_text(audio_path: str, output_path: str):
-    config = aai.TranscriptionConfig(
-        speaker_labels=True,
-    )
-    transcriber = aai.Transcriber()
 
-    transcript = transcriber.transcribe(audio_path, config=config)
+    result = ASR_MODEL.transcribe(audio_path)
 
     conversation = []
 
-    for utterance in transcript.json_response['utterances']:
-        conversation.append({
-            utterance['speaker']: utterance['text']
-        })
+    for segment in result["segments"]:
+        conversation.append(segment["text"])
 
+    exit()
+    
     with open(output_path, 'w') as f:
-        json.dump(conversation, f, indent=4)
+        f.write("\n".join(conversation))
 
     return conversation
+
+
+if __name__ == "__main__":
+    transcript_audio_to_text("data/audios/2025-04-08 09-02-19_audio.mp3", "data/transcripts/testing_gemini.log")
+
+
+
+
+
+
